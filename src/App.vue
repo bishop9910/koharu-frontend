0ba602a09aa01ae40ab9c13e5d9f91eb2c6ea995
Wangt <3180454348@qq.com>
2026-09-12 12:39:15 +0800

    w

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <!-- App.vue -->
 <script setup lang="ts">
+import APITestLayout from './components/layout/APITestLayout.vue';
 import UITestLayout from './components/layout/UITestLayout.vue';
+
 </script>
 
 <template>
-  <UITestLayout/>
+  <APITestLayout/>
+
 </template>
 
 <style scoped></style>
--- src/components/layout/APITestLayout.vue
+++ src/components/layout/APITestLayout.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+import { login } from '@/api/auth';
+import { onMounted } from 'vue';
+onMounted(()=>{
+    login({username:"admin",password:"34bd974ac25d931001d06695ec6b54a9",encrypted:false}).then((res)=>{
+        console.log(res);
+    })
+})
 
 </script>
 
