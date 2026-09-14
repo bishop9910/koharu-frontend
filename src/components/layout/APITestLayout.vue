da02c401128da9168fa80bc6f4fd63631eac4f60
bishop9910 <bishop9910@163.com>
2026-09-14 10:09:49 +0800

    t

--- src/components/layout/APITestLayout.vue
+++ src/components/layout/APITestLayout.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { login } from '@/api/auth';
+import { getPublicKey, login } from '@/api/auth';
 import { onMounted } from 'vue';
 onMounted(()=>{
     login({username:"admin",password:"34bd974ac25d931001d06695ec6b54a9",encrypted:false}).then((res)=>{
         console.log(res);
+    })
+
+    getPublicKey().then((res)=>{
+        console.log(res)
     })
 })
 
